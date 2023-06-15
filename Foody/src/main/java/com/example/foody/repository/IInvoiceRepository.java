c916b479f30f43e20daaa700061e1c60b935633e
Meeloow <quockha081122@gmail.com>
2023-06-15 11:27:15 +0700

    test

--- Foody/src/main/java/com/example/foody/repository/IInvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.foody.repository;
-
-
-import com.example.foody.entity.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface IInvoiceRepository extends JpaRepository<Invoice, Long>{
-}
--- Foody/src/main/java/com/example/foody/repository/IItemInvoiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.foody.repository;
-
-import com.example.foody.entity.ItemInvoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface IItemInvoiceRepository extends JpaRepository<ItemInvoice, Long>{
-}
